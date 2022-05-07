1bdea37b7598a7ac004d8c2dcae24b04e774da92
Ary Plateira <ary_plateira@outlook.com>
2022-05-07 18:33:57 -0300

    teste

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
       <header className="App-header" style={{ backgroundColor: cordefundo ? "grey" : "white" }}>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          aryelly <code>src/App.js</code> and save to reload.
+          leo <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
